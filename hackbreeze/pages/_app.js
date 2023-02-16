ffdc2d9a146bf47705e98e120bd6fbb8b81621bd
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-02-16 17:06:13 +0530

    add basic layout

--- hackbreeze/pages/_app.js
+++ hackbreeze/pages/_app.js
@@ -1,12 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Layout from "../components/UI/Layout";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
