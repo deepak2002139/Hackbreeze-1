35c86e2b50305d2b1b2770c4dd149a51f5bbf7e1
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2023-02-16 17:02:36 +0530

    add basic struct

---
+++ hackbreeze/pages/_app.js
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../styles/globals.css";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
